0ee88e14690bb7e515d37a0547de8cbe3743d57f
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-09-26 08:11:59 +0200

    Set setup runtime version 0.2.2

--- installer/DynReportSystem.Setup/Program.cs
+++ installer/DynReportSystem.Setup/Program.cs
@@ -7,7 +7,7 @@ using System.Text.Json;
 using Microsoft.Win32;
 
 const string ProductName = "DynReport System";
-const string Version = "0.2.1";
+const string Version = "0.2.2";
 const string SiteName = "DynReportSystem";
 const string AppPool = "DynReportSystem";
 const int DefaultPort = 47131;
